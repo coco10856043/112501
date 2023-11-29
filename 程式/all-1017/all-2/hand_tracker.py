197622a17f52977bfbd7054c2a961755f2a2a3c2
10856007 <10856007@ntub.edu.tw>
2023-11-29 19:24:42 +0800

    九月到十一月檔案

---
+++ 程式/all-1017/all-2/hand_tracker.py
@@ -0,0 +1,225 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import cvzone
+import time
+import subprocess
+import sys, mysql.connector
+
+# -----------------------------------------
+# 跳轉程式
+# -----------------------------------------
+
+# 初始化 MediaPipe Hand 模型
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+
+# 打開攝像頭
+cap = cv2.VideoCapture(0)
+
+# 設定初始視窗大小
+initial_width, initial_height = 800, 600
+cv2.namedWindow('Hand Tracking', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('Hand Tracking', initial_width, initial_height)
+
+result_from_get_account = sys.argv[1]
+
+# 載入要顯示的圖片
+image_path1 = 'game_png/Rat-1.png'  
+image_path2 = 'game_png/baseball.png'  
+image_path3 = 'game_png/cat_1.png'  
+image1 = cv2.imread(image_path1, -1)  # -1 可以讀取包含透明通道的圖片
+image2 = cv2.imread(image_path2, -1)  
+image3 = cv2.imread(image_path3, -1)  
+# 載入通知
+image_path4 = 'game_png/text-1.png' 
+image4 = cv2.imread(image_path4, -1)
+image_path5 = 'game_png/text-2.png' 
+image5 = cv2.imread(image_path5, -1) 
+image_path6 = 'game_png/text-3.png'  
+image6 = cv2.imread(image_path6, -1) 
+
+
+# 開始計時
+start_time = None
+
+while cap.isOpened():
+    # 讀取一幀圖像
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to capture image")
+        break
+
+    # 將圖像水平翻轉
+    frame_flipped = cv2.flip(frame, 1)
+
+    # 將翻轉後的圖像轉換為 RGB 格式
+    frame_rgb = cv2.cvtColor(frame_flipped, cv2.COLOR_BGR2RGB)
+
+    # 進行手部檢測
+    results = hands.process(frame_rgb)
+
+    # 判斷手的位置
+    hand_position = 0
+
+    if results.multi_hand_landmarks:
+        landmarks = results.multi_hand_landmarks[0].landmark
+        hand_x = landmarks[0].x  # 假設使用第一個手的第一個關節位置來判斷手的位置
+
+        # 判斷手的位置
+        if hand_x < 0.33:
+            hand_position = 1
+            image_to_display = image1  # 圖片1
+            #-----------------------------------------
+            image_resized_4 = cv2.resize(image4, (640, 100))  # 調整圖片大小
+
+            # 設置第四張圖片在相機捕獲的視訊幀的位置（放在底部中心）
+            bottom_left = (0, 380)
+            bottom_right = (bottom_left[0] + image_resized_4.shape[1], bottom_left[1] + image_resized_4.shape[0])
+
+            # 將第四張圖片放入相機捕獲的視訊幀中（考慮圖片的透明度）
+            overlay_4 = frame_flipped.copy()
+            overlay_4[bottom_left[1]:bottom_right[1], bottom_left[0]:bottom_right[0]] = image_resized_4
+
+            alpha = 1  # 圖片透明度
+            cv2.addWeighted(overlay_4, alpha, frame_flipped, 1 - alpha, 0, frame_flipped)
+            #-----------------------------------------
+            # 如果 start_time 是 None，則啟動計時器
+            if start_time is None:
+                start_time = time.time()
+
+            # 超過五秒 觸發跳轉
+            if time.time() - start_time > 5:
+                
+                result_from_get_account = sys.argv[1]
+                # 並關閉所有視窗
+                cap.release()
+                cv2.destroyAllWindows()
+                
+                # 執行另一個 Python 檔案
+                subprocess.run(["python", "whack-a-mole.py", result_from_get_account])
+                #subprocess.run(["python", "whack-a-mole.py"])
+            #-----------------------------------------
+
+        elif hand_x > 0.67:
+            hand_position = 3
+            image_to_display = image3  # 圖片3
+            #-----------------------------------------
+            image_resized_6 = cv2.resize(image6, (640, 100))  # 圖片大小
+
+            # 設置第四張圖片在相機捕獲的視訊幀的位置（放在底部中心）
+            bottom_left = (0, 380)
+            bottom_right = (bottom_left[0] + image_resized_6.shape[1], bottom_left[1] + image_resized_6.shape[0])
+
+            # 將第四張圖片放入相機捕獲的視訊幀中（考慮圖片的透明度）
+            overlay_6 = frame_flipped.copy()
+            overlay_6[bottom_left[1]:bottom_right[1], bottom_left[0]:bottom_right[0]] = image_resized_6
+
+            alpha = 1  # 圖片透明度
+            cv2.addWeighted(overlay_6, alpha, frame_flipped, 1 - alpha, 0, frame_flipped)
+            #-----------------------------------------
+            # 如果 start_time 是 None，則啟動計時器
+            if start_time is None:
+                start_time = time.time()
+
+            # 超過五秒 觸發跳轉
+            if time.time() - start_time > 5:
+                result_from_get_account = sys.argv[1]
+                # 並關閉所有視窗
+                cap.release()
+                cv2.destroyAllWindows()
+
+                # 執行另一個 Python 檔案
+                subprocess.run(["python", "run.py", result_from_get_account])
+                #subprocess.run(["python", "run.py"])
+            #-----------------------------------------
+        else:
+            hand_position = 2
+            image_to_display = image2  # 位置2對應圖片2
+            #-----------------------------------------
+            image_resized_5 = cv2.resize(image5, (640, 100))  # 調整圖片大小
+
+            # 設置第四張圖片在相機捕獲的視訊幀的位置（放在底部中心）
+            bottom_left = (0, 380)
+            bottom_right = (bottom_left[0] + image_resized_5.shape[1], bottom_left[1] + image_resized_5.shape[0])
+
+            # 將第四張圖片放入相機捕獲的視訊幀中（考慮圖片的透明度）
+            overlay_5 = frame_flipped.copy()
+            overlay_5[bottom_left[1]:bottom_right[1], bottom_left[0]:bottom_right[0]] = image_resized_5
+
+            alpha = 1  # 圖片透明度
+            cv2.addWeighted(overlay_5, alpha, frame_flipped, 1 - alpha, 0, frame_flipped)
+            #-----------------------------------------
+            # 如果 start_time 是 None，則啟動計時器
+            if start_time is None:
+                start_time = time.time()
+
+            # 超過五秒 觸發跳轉
+            if time.time() - start_time > 5:
+
+                result_from_get_account = sys.argv[1]
+                # 並關閉所有視窗
+                cap.release()
+                cv2.destroyAllWindows()
+
+                # 執行另一個 Python 檔案
+                subprocess.run(["python", "catch-ball.py", result_from_get_account])
+                #subprocess.run(["python", "Pose.py"])
+
+            # else:
+            #     # 手不被偵測到時，重置計時器
+            #     start_time = None
+            #-----------------------------------------
+
+
+        # 調整圖片大小以符合視訊畫面
+        image_resized = cv2.resize(image_to_display, (100, 100))  # 調整圖片大小
+
+        # 設置圖片在相機捕獲的視訊幀的位置
+        top_left = ((hand_position - 1) * 250 + (100 - image_resized.shape[1]) // 2, 0)
+        bottom_right = (top_left[0] + image_resized.shape[1], top_left[1] + image_resized.shape[0])  # 計算圖片右下角位置
+
+        # 將圖片放入相機捕獲的視訊幀中（考慮圖片的透明度）
+        # if image_resized.shape[2] == 3:
+        #     alpha_channel = np.ones((image_resized.shape[0], image_resized.shape[1]), dtype=image_resized.dtype) * 255
+        #     image_resized = cv2.merge((image_resized, alpha_channel))
+        # overlay = frame_flipped.copy()
+        # overlay[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]] = image_resized
+        # frame_flipped = cvzone.overlayPNG(frame_flipped, image_resized, (top_left, bottom_right))
+        # 设置图像在相机捕获的视频帧的位置（考虑图像的透明度）
+        alpha_channel = image_resized[:, :, 3] / 255.0  # 提取透明通道并归一化
+        overlay = image_resized[:, :, :3]  # 提取RGB通道
+
+        # 设置图像位置
+        roi = frame_flipped[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+
+        # 将图像叠加到视频帧上
+        for c in range(3):
+            roi[:, :, c] = (1 - alpha_channel) * roi[:, :, c] + alpha_channel * overlay[:, :, c]
+        
+        alpha = 1  # 圖片透明度
+        # cv2.addWeighted(overlay, alpha, frame_flipped, 1 - alpha, 0, frame_flipped)
+
+    # 在畫面上顯示手的位置
+    # cv2.putText(frame_flipped, str(hand_position), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    # 在視窗中畫兩條直線
+    line1_start, line1_end = (initial_width // 4, 0), (initial_width // 4, initial_height - 220)
+    line2_start, line2_end = (2 * initial_width // 3 - 100, 0), (2 * initial_width // 3 - 100, initial_height - 220)
+    cv2.line(frame_flipped, line1_start, line1_end, (255, 255, 255), 1)
+    cv2.line(frame_flipped, line2_start, line2_end, (255, 255, 255), 1)
+
+    # 顯示結果
+    cv2.imshow('Hand Tracking', frame_flipped)
+
+    # 按 'ESC' 鍵退出循環
+    if cv2.waitKey(1) & 0xFF == 27:
+        # 並關閉所有視窗(失敗)
+        cap.release()
+        cv2.destroyAllWindows()                
+        # 執行另一個 Python 檔案
+        subprocess.run(["python", "AR_home.py", result_from_get_account])
+
+# 釋放攝像頭並關閉所有窗口
+cap.release()
+cv2.destroyAllWindows()
---
+++ 程式/all-1017/all-2/whack-a-mole.py
@@ -0,0 +1,235 @@
+import cv2
+import mediapipe as mp
+import cvzone
+import threading
+import numpy as np
+import time
+import random
+import pygame
+import subprocess
+import sys, mysql.connector
+# ------------------------------
+# 換圖片(X)
+# 視窗大小800x600(X)
+# ------------------------------
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+# 遊戲參數
+score = 0
+TIMER_SECONDS = 60  # 設定計時器時間（秒）
+start_time = time.time()
+last_change_time = time.time()
+
+# 遊戲圖片參數
+circle_radius = 50
+circle_color = (0, 255, 255)  # 黃色
+circle_position = (0, 0)
+
+# 要蓋上的圖片
+image_to_overlay = cv2.imread('game_png/Rat-1.png', cv2.IMREAD_UNCHANGED)  # 替換成您的圖片路徑
+image_to_overlay = cv2.resize(image_to_overlay, (100, 100))  # 調整圖片大小
+if image_to_overlay.shape[2] == 4:  # 检查图像是否有 alpha 通道
+    alpha_channel_r = image_to_overlay[:, :, 3] / 255.0
+    overlay = image_to_overlay[:, :, :3]
+else:
+    alpha_channel_r = np.ones((image_to_overlay.shape[0], image_to_overlay.shape[1]), dtype=image_to_overlay.dtype)
+    overlay = image_to_overlay
+# image_hammer = cv2.imread('game_png/hammer-1.png', cv2.IMREAD_UNCHANGED)  # 替換成您的圖片路徑
+# image_hammer = cv2.resize(image_hammer, (100, 100))  # 調整圖片大小
+# if image_hammer.shape[2] == 4:  # 检查图像是否有 alpha 通道
+#     alpha_channel_h = image_hammer[:, :, 3] / 255.0
+#     hammer = image_hammer[:, :, :3]
+# else:
+#     alpha_channel_h = np.ones((image_hammer.shape[0], image_hammer.shape[1]), dtype=image_hammer.dtype)
+#     hammer = image_hammer
+image_hammer = cv2.imread('game_png/hammer-1.png', cv2.IMREAD_UNCHANGED)
+if image_hammer.shape[2] == 3:  # 如果是三通道，加入透明通道
+    alpha_channel = np.ones((image_hammer.shape[0], image_hammer.shape[1]), dtype=image_hammer.dtype) * 255
+    image_hammer = cv2.merge((image_hammer, alpha_channel))
+
+result_from_get_account = sys.argv[1]
+result_list = result_from_get_account.strip('[()]').split(', ')
+result_list = [item.strip("'") for item in result_list]
+
+# 資料庫連線參數
+db_config = {
+    'host': '140.131.114.140',  # 修改成你的 MySQL 伺服器 IP
+    'port': 3306,         # 修改成你的 MySQL 伺服器 Port
+    'user': '112501',        # 修改成你的 MySQL 使用者帳號
+    'password': 'SportsWorld_112501',    # 修改成你的 MySQL 使用者密碼
+    'database': '112-112501',  # 修改成你的資料庫名稱
+}
+
+# 連接到資料庫
+conn = mysql.connector.connect(**db_config)
+
+# 開啟攝像頭
+camera = 0
+cap = cv2.VideoCapture(camera)
+
+# 設定計時器
+def start_timer():
+    # 設定計時器初始值
+    global timer_count
+    timer_count = TIMER_SECONDS
+
+    # 每秒減少計時器值，直到計時器為 0
+    while timer_count > 0:
+        timer_count -= 1
+        print('Time:', timer_count)
+        time.sleep(1)
+
+    # 顯示結束畫面
+    # show_final_screen()
+
+# 啟動計時器
+threading.Thread(target=start_timer).start()
+
+# 初始化MediaPipe手部檢測工具
+with mp_hands.Hands(
+        model_complexity=0,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
+
+    while cap.isOpened():
+        success, image = cap.read()
+
+        if not success:
+            print("Ignoring empty camera frame.")
+            continue
+
+        # 水平翻轉畫面，形成鏡像效果
+        image = cv2.flip(image, 1)
+
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
+
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # 在右上角顯示分數
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(image, f'Score: {score}', (0, 30), font, 1, (0, 0, 0), 3, cv2.LINE_AA)
+        # 在右上角顯示倒數計時
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(image, f'Time left: {timer_count}', (image.shape[1] - 210, 30), font, 1, (0, 0, 0), 3, cv2.LINE_AA)
+        
+        # cv2.circle(image, circle_position, circle_radius, circle_color, -1)
+        # 每 3 秒換一次圖片位置
+        if time.time() - last_change_time > 3:
+            last_change_time = time.time()
+            circle_position = (random.randint(0, image.shape[1] - 2 * circle_radius),
+                               image.shape[0] - circle_radius)
+            
+         # 繪製圖片
+        if image_to_overlay is not None:
+            # 計算圖片的位置
+            img_y, img_x = circle_position[1] - 50, circle_position[0] - 50
+            # 確保圖片不會超出畫面邊界
+            img_y = max(0, min(img_y, image.shape[0] - 100))
+            img_x = max(0, min(img_x, image.shape[1] - 100))
+            
+            # 將圖片蓋在畫面上
+            # image[img_y:img_y+100, img_x:img_x+100] = image_to_overlay
+            # 设置图像在相机捕获的视频帧的位置（考虑图像的透明度）
+            alpha_channel_r = image_to_overlay[:, :, 3] / 255.0  # 提取透明通道并归一化
+            overlay = image_to_overlay[:, :, :3]  # 提取RGB通道
+
+            # 设置图像位置
+            roi_r = image[img_y:img_y+100, img_x:img_x+100]
+
+            # 将图像叠加到视频帧上
+            for c in range(3):
+                roi_r[:, :, c] = (1 - alpha_channel_r) * roi_r[:, :, c] + alpha_channel_r * overlay[:, :, c]
+
+        if results.multi_hand_landmarks:
+            for i, hand_landmarks in enumerate(results.multi_hand_landmarks):
+                x = int(hand_landmarks.landmark[9].x * image.shape[1])
+                y = int(hand_landmarks.landmark[9].y * image.shape[0])
+                cv2.circle(image, (x, y), 5, (0, 0, 255), -1)
+                image = cvzone.overlayPNG(image, image_hammer, (x, y))
+                 # 繪製圖片
+                # if image_hammer is not None:                    
+                #     # 將圖片蓋在畫面上
+                #     # image[y:y+100, x:x+100] = image_hammer
+                #     # 设置图像在相机捕获的视频帧的位置（考虑图像的透明度）
+                #     alpha_channel_h = image_hammer[:, :, 3] / 255.0  # 提取透明通道并归一化
+                #     hammer = image_hammer[:, :, :3]  # 提取RGB通道
+
+                #     # 设置图像位置
+                #     roi_h = image[y:y+100, x:x+100]
+
+                #     # 將圖片叠加到视频帧上
+                #     for c in range(3):
+                #         roi_h[:, :, c] = (1 - alpha_channel_h) * roi_h[:, :, c] + alpha_channel_h * hammer[:, :, c]
+
+                # 檢查是否達到一定高度
+                touch_start_time = 0
+                if y < circle_position[1] + circle_radius*3:
+                    touch_start_time = time.time()
+                # 檢查是否確實打到地鼠
+                if (y < circle_position[1] + circle_radius and y > circle_position[1] - circle_radius) and (x > circle_position[0] - circle_radius and x < circle_position[0] + circle_radius):
+                    if touch_start_time:
+                        touch_time = time.time() - touch_start_time
+                        if touch_time < 1:
+                            score += 1
+                            print(f"得分 +1，總分：{score}")
+                            circle_position = (random.randint(0, image.shape[1] - 2 * circle_radius),
+                                            image.shape[0] - circle_radius)
+                            touch_start_time = 0
+
+        # 顯示遊戲畫面
+        cv2.imshow('Whack-a-Mole Game', image)
+
+        # 檢查遊戲時間
+        if timer_count == 0:
+            print("遊戲結束，總分：", score)
+
+            initface_cursor= conn.cursor()
+            query1 = "INSERT INTO game (`user_id`, `game_type`, `game_time`, `scores`) VALUES (%s, '1', current_timestamp(), %s);"
+            values = (result_list[0], score)
+            
+            initface_cursor.execute(query1, values)
+            conn.commit()
+            break
+
+        # 按下Esc鍵結束遊戲
+        if cv2.waitKey(1) == 27:
+            timer_count = 0
+            break
+
+# 釋放資源
+cap.release()
+cv2.destroyAllWindows()
+
+# ------------------------------
+# 分數頁面
+# ------------------------------
+pygame.init()
+
+end_window = pygame.display.set_mode((800, 600))
+background_img = pygame.image.load("game_png/score_bg.png")
+end_window.blit(background_img, (0, 0))
+
+end_font = pygame.font.SysFont(None, 60)
+end_text = end_font.render(f"Game Over! Your Score: {score}", True, (255, 255, 255))
+text_rect = end_text.get_rect(center=end_window.get_rect().center)  # 至中
+end_window.blit(end_text, text_rect)
+
+pygame.display.update()
+# ------------------------------
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+# 關閉頁面
+cap.release()
+pygame.quit()
+
+subprocess.run(["python", "AR_home.py", result_from_get_account])  # 跳轉
+# ------------------------------
